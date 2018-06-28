cf4d7051f87264864e03f508bc85a59fb3c66b17
Matti Salokangas <matti.salokangas@gmail.com>
2018-06-28 14:44:21 -0700

    Clean up

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import Login from './Login';
 import Home from './Home';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
--- server.js
+++ server.js
@@ -4,8 +4,11 @@ const jsonWebToken = require('jsonwebtoken');
 const app = express();
 const router = express.Router();
 
-const secret = 'super-secret';
+// Salt used with hash when creating JWT token
+const secret = process.env.SECRET;
 
+// Create JWT token
+// This function would be better placed in another file
 function createJwtToken(user) {
   return jsonWebToken.sign(
     {
@@ -20,6 +23,17 @@ function createJwtToken(user) {
   );
 }
 
+// We also pass the jwt middleware to authenticate our routes that start with /api
+// Note the usage of "unless" to allow for the api/auth route to be hit
+// without an Authorization header
+app.use('/api', jwt({ secret }).unless({
+  path: [ '/api/auth' ],
+}));
+
+// Route to authenticate user
+// You would typically retrieve the username/password/apikey and authenticate that against your database/etc.
+// Then you would call the createJwtToken(...) with the data you'd like to have available in the future
+// for subsequent requests for authorization and identification.
 router.post('/auth', (req, res) => {
   const token = createJwtToken({
     email: 'test@test.com'
@@ -29,19 +43,22 @@ router.post('/auth', (req, res) => {
   });
 });
 
+// Example route
 router.get('/dataz', (req, res) => {
+  // The JWT middleware adds the "user" object 
+  // to the req when there is a valid jwt token
+  console.log('req.user', req.user);
+  
   res.send([
     { id: 1, name: 'test 1' },
     { id: 2, name: 'test 2' },
     { id: 3, name: 'test 3' },
   ]);
-})
+});
 
+// Tell Express to route requests coming in at /api to use
+// the routes we just setup.
 app.use('/api', router);
-
-app.use('/api', jwt({ secret, }).unless({
-  path: [ '/api/auth' ],
-}));
 
 // Express only serves static assets in production
 if (process.env.NODE_ENV === 'production') {
@@ -56,7 +73,7 @@ const port = process.env.PORT || 5001;
 
 app.listen(port, (err) => {
   if (err) {
-    console.log('err starting server', err);
+    console.log(`Err starting server: ${err}`, err);
   } else {
     console.log(`Serving from port ${port}`);
   }
